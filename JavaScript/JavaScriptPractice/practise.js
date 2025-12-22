57aeaf75f8b39fc401eb83dfa37d82a99758a918
Nithish <nithissjagan@gmail.com>
2025-12-22 16:23:33 +0530

    Initial commit

---
+++ JavaScript/JavaScriptPractice/practise.js
@@ -0,0 +1,18 @@
+console.log("Hello")
+
+let a = 4;
+a = 5
+
+var b = "Hello"
+var b = "ys"
+
+let required = false
+
+console.log(b)
+console.log(a)
+console.log(a,b)
+
+const c = 5.6;
+console.log(c)
+console.log(typeof(c))
+console.log(typeof(required))
